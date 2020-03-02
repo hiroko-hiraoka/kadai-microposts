539a004cc4dd49501120a92a82f402ca2729f0fd
hiroko.hiraoka <chibichibikichi@gmail.com>
2020-03-02 16:53:11 +0000

    delete button

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -10,10 +10,10 @@
                     <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                 </div>
                 <div>
-                    @if (Auth::id() === $micropost->userid)
-                        {!! Form::open(['route' => ['microposts.destroy', $microposst->id], 'method' => 'delete']) !!}
+                    @if (Auth::id() === $micropost->user_id)
+                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-                        {!! Form**close() !!}
+                        {!! Form::close() !!}
                     @endif
                 </div>
             </div>
